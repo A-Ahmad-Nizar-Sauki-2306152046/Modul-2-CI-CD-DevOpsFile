83c260a538d84e85df82063ebd35c14984db42e4
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-02-24 00:14:07 +0700

    fix: remove unused empty setUp method in ProductRepositoryTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -17,10 +17,6 @@ class ProductRepositoryTest {
    @InjectMocks
    ProductRepository productRepository;
 
-   @BeforeEach
-   void setUp() {
-   }
-
    @Test
    void testCreateProductWithoutId() {
       Product product = new Product();
